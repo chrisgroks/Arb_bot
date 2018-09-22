6ff4127051444e32f8ee13c8e500a758c100df8e
diaz9797 <diaz9797@gmail.com>
2018-09-22 12:57:56 -0500

    Update login.py

--- login.py
+++ login.py
@@ -11,8 +11,8 @@ import gdax
 # b64secret = API secret key
 # passphrase = passphrase from GDAX
 
-key = '9db83dae13c7e59a07e131d4ed2a3c9b'
-b64secret = 'ZboNNE/esWGreKU1/Y7CIWlSo36hwO6Gk6e4uwkfPY1lOsSnF8nK67Sg2wTFEA7f6V/dFjdFifgiAg1U+Y0WOw=='
-passphrase = '4arsa9zrqkbs'
+key = 
+b64secret = 
+passphrase = 
 
 auth_client = gdax.AuthenticatedClient(key, b64secret, passphrase)
